ec72e3ac85bb99472a9ed1911a1398a15b088918
kasatkasss <lysyaloslos@gmail.com>
2021-01-13 00:15:03 +0300

    first commit

---
+++ Arrays/Arrays/Browser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Arrays //??
+{
+    public class Browser // Browser?!
+    {
+        public enum Operations
+        {
+            Add = 1,
+            Subtract,
+            Multiply,
+            Devide
+        }
+
+        public int Calculate(int x, int y, Operations operationType) //why the method is static?
+        {
+            int result = 0;
+            switch (operationType)
+            {
+                case Operations.Add:
+                    result = x + y;
+                    break;
+                case Operations.Subtract:
+                    result = x - y;
+                    break;
+                case Operations.Multiply:
+                    result = x * y;
+                    break;
+                case Operations.Devide:
+                    result = x / y;
+                    break;
+            }
+            return result;
+        } 
+
+        public Operations GetOperationType(string operationIndex)
+        {
+            Operations result = 0;
+            switch(operationIndex)
+            {
+                case "Сложение":
+                    result = Operations.Add;
+                    break;
+                case "Вычитание":
+                    result = Operations.Subtract;
+                    break;
+                case "Умножение":
+                    result = Operations.Multiply;
+                    break;
+                case "Деление":
+                    result = Operations.Devide;
+                    break;
+            }
+            return result;
+        }
+        
+    }
+}
---
+++ Arrays/Arrays/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Arrays
+{
+    public class Calculator 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            int firstNum = Convert.ToInt32(Console.ReadLine()); // use camelCase - Done
+            Console.WriteLine("Введите второе число:");
+            int secondNum = Convert.ToInt32(Console.ReadLine()); // use camelCase - Done
+            string operationIndex;
+            string[] array = { "Сложение", "Вычитание", "Умножение", "Деление" }; // incorrect logic of condition. this block of code doesn't work as expected - Done.
+            do 
+            {
+                Console.WriteLine("Выберите тип операции: \nСложение \nВычитание \nУмножение \nДеление");
+                operationIndex = Console.ReadLine();
+            }
+            while (!array.Contains(operationIndex));
+            Browser calculator = new Browser();
+
+            Browser.Operations operationType = calculator.GetOperationType(operationIndex);
+            int result = calculator.Calculate(firstNum, secondNum, operationType);
+            Console.WriteLine($"результат операции равен {result}");
+            Console.ReadKey();
+        }
+    }
+}
