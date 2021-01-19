7cf69243359340438ce6b3f0c4048a5d6d338492
kasatkasss <lysyaloslos@gmail.com>
2021-01-19 20:18:27 +0300

    Renaming variables and method Area()
    side -> square, sides -> rectangle, Area() -> GetArea(), change type of
    entered values to float

---
+++ AreaCalculations/AreaCalculations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AreaCalculations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square();
+            Rectangle rectangle = new Rectangle();
+            Console.WriteLine("Выберите фигуру, площадь которой хотите вычислить: \nКвадрат \nПрямоугольник");
+            string figure = Console.ReadLine();
+            if(figure == "Квадрат")
+            {
+                Console.WriteLine("Введите длину стороны квадрата:");
+                square.side = Convert.ToInt32(Console.ReadLine());
+                int squareResult = square.GetArea();
+                Console.WriteLine($"Площадь квадрата равна: {squareResult}");
+
+                
+            }
+            else if(figure == "Прямоугольник")
+            {
+                Console.WriteLine("Введите длину:");
+                rectangle.height = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите ширину:");
+                rectangle.width = Convert.ToInt32(Console.ReadLine());
+                int rectangleResult = rectangle.GetArea();
+                Console.WriteLine($"Площадь Прямоугольника равна: {rectangleResult}");
+            }
+            else
+            {
+                Console.WriteLine("Вы неправильно ввели фигуру");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ AreaCalculations/AreaCalculations/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaCalculations
+{
+    public class Rectangle : Figure
+    {
+        public int width;
+        public int height;
+        public override int GetArea()
+        {
+            return height * width;
+        }
+    }
+}
